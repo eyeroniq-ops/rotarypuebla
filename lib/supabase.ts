e6a27dfd4b0eb3dd35d75a665f8f83e30c572d9e
k3e <k3e@MacBook-Air-de-k3e.local>
2026-02-06 13:38:13 -0600

    fix: Update Supabase API key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // Credentials provided by user
 const supabaseUrl = 'https://kfhegcfkhhqggdvvunod.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFub24iLCJpYXQiOjE3NzAzOTk1MzQsImV4cCI6MjA4NTk3NTUzNH0.w7vcKw7API3tYC_EZSo1Fzz28pYo3_7EF6X_zZ_x5RM';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmaGVnY2ZraGhxZ2dkdnZ1bm9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAzOTk1MzQsImV4cCI6MjA4NTk3NTUzNH0.w7vcKw7API3tYC_EZSo1Fzz28pYo3_7EF6X_zZ_x5RM';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
